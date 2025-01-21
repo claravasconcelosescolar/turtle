1b2adfcd8ebd6d4c4cf219036e67af8f3a6e6968
claravasconcelosescolar <vasconcelos.clara@escolar.ifrn.edu.br>
2025-01-21 07:50:43 -0300

    Add files via upload

---
+++ flores.py
@@ -0,0 +1,22 @@
+import turtle
+
+esguicho = turtle.Turtle()
+
+# definindo a função
+def desenhe_uma_flor():
+    for _ in range(6):
+        for _ in range(8):
+            esguicho.forward(20)
+            esguicho.right(30)
+        esguicho.right(60)
+
+def pula (pixels):
+    esguicho.penup()
+    esguicho.forward(pixels)
+    esguicho.pendown()
+
+desenhe_uma_flor()
+pula(150)
+desenhe_uma_flor()
+pula(150)
+desenhe_uma_flor()
---
+++ suavez.py
@@ -0,0 +1,16 @@
+import turtle
+
+esguicho = turtle.Turtle()
+
+for i in range (17) :
+ esguicho.right(40)
+ esguicho.forward(60)
+ esguicho.left(30)
+ esguicho.forward(50)
+ esguicho.left(150)
+ esguicho.forward(80)
+ esguicho.left(30)
+ esguicho.forward(60)
+ 
+turtle.exitonclick()
+
